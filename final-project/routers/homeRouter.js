2571662a38be5305632d50404ece1fa4e6fc2fa2
Codese <thienhadenhatvonhi@gmail.com>
2019-12-04 21:01:55 +0700

    hoc javascript nang cao

--- final-project/routers/homeRouter.js
+++ final-project/routers/homeRouter.js
@@ -9,6 +9,7 @@ const {
 
 Router.get("/", async (req, res) => {
   let data = await readAllProduct()
+  console.log(data)
   let renderHTML = ""
   data.forEach(element => {
     renderHTML = renderHTML +
@@ -37,3 +38,35 @@ Router.get("/", async (req, res) => {
 
 
 module.exports = Router;
+
+
+var a = [5, 8, 6, 4, 9, 2, 4, 7, 8, 6, 4]
+
+for (let i = 0; i < a.length; i++) {
+  lamgi(a[i])
+}
+
+a.forEach((value, index) => {
+  console.log("FE: " + value + " index " + index)
+})
+var amap = a.map((value, index,array) => {
+  if(index==array.length-1) return ""
+  else return "Gia tri " + value + " chi so " + index
+})
+console.log(amap) 
+var areduce = a.reduce((pre,cur)=>{
+  return pre + cur;
+})
+console.log(areduce)
+function lamgi(x) {
+
+}
+
+function afunc(a=1,b=2,c=3,d=4){
+  console.log("co a: " +a)
+  console.log("co b: " +b)
+  console.log("co c: " +c)
+  console.log("co d: " +d)
+}
+
+afunc(10,undefined,30)
